09b0acd718fcd81f943bff79c32a76b11071bc63
markgolubev <markflyingbear@gmail.com>
2022-12-16 11:53:07 +0000

    Initial Commit

---
+++ Whitehouse Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whitehouse Petitions
+//
+//  Created by Марк Голубев on 16.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
